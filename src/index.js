710d726e3ada5424517853178e34e3517ba9f72a
Luis Vinicius <luisvinicius0906@gmail.com>
2021-06-25 18:01:44 -0300

    Add provider passing the store

--- src/index.js
+++ src/index.js
@@ -2,10 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import Routes from './Router';
+import { Provider } from 'react-redux';
+import { store } from './state/index';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Routes />
+    <Provider store={store}>
+      <Routes />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
