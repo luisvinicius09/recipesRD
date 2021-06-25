acaac70dc8798fe502033e834b3144dc24dbdd82
Luis Vinicius <luisvinicius0906@gmail.com>
2021-06-25 18:13:25 -0300

    Remove useless files

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import './assets/index.css';
 import Routes from './Router';
 import { Provider } from 'react-redux';
 import { store } from './state/index';
