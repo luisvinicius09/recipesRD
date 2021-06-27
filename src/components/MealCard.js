f66f1680ac72e6a88f2f95e66d5e474f7596b590
Luis Vinicius <luisvinicius0906@gmail.com>
2021-06-27 16:55:25 -0300

    Update path on link

--- src/components/MealCard.js
+++ src/components/MealCard.js
@@ -2,7 +2,7 @@ import { Link } from 'react-router-dom';
 
 const MealCard = ({ meal, id, image }) => (
   <>
-    <Link>
+    <Link to={"/meals/" + id}>
       <img src={image} alt={meal}/>
       <p>{meal}</p>
     </Link>
