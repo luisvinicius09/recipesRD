014524524aa271d6b1fe1955cc72e1d20a064260
Luis Vinicius <luisvinicius0906@gmail.com>
2021-06-27 01:22:35 -0300

    Update App component to render Home page

--- src/components/App.js
+++ src/components/App.js
@@ -1,28 +1,9 @@
-import React, { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { fetchData } from '../state/actions';
+import Home from './Home';
 
-const App = () => {
-  const dispatch = useDispatch();
-  const state = useSelector((state) => state);
-
-  useEffect(() => {
-    dispatch(fetchData());
-  }, [dispatch]);
-
-  const renderRecipes = () => {
-    if (state.loading) {
-      return <h2>Loading...</h2>;
-    }
-    return state.data.items.map((recipe) => recipe.strCategory);
-  }
-
-  return (
-    <div>
-      <h1>Hello to the real world</h1>
-      { renderRecipes() }
-    </div>
-  );
-};
+const App = () => (
+  <>
+    <Home />
+  </>
+)
 
 export default App;
