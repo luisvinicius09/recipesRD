251da2ff46708bc5dc1997c87de39e89ee220927
Luis Vinicius <luisvinicius0906@gmail.com>
2021-06-27 18:32:58 -0300

    Add logic to display data from meal by id

--- src/components/meal/Meal.js
+++ src/components/meal/Meal.js
@@ -1,5 +1,29 @@
-const Meal = () => (
-  <h1>Test</h1>
-);
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { useParams } from 'react-router';
+import { fetchMealById } from '../../state/actions/index'
+
+const Meal = () => {
+  const dispatch = useDispatch();
+  const { id } = useParams();
+  const state = useSelector((state) => state);
+
+  useEffect(() => {
+    dispatch(fetchMealById(id))
+  }, [dispatch, id]);
+
+  const meal = state.data.items[0];
+
+  if (!state.data.items[0]) {
+    return <h1>Loading...</h1>;
+  }
+
+  return (
+    <>
+      <h1>{ meal.strMeal }</h1>
+      <img src={ meal.strMealThumb } alt={ meal.strMeal }/>
+    </>
+  )
+};
 
 export default Meal;
