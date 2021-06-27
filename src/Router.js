fd10b4da7dc266b6dd415fc167f05bc85dcea573
Luis Vinicius <luisvinicius0906@gmail.com>
2021-06-27 15:53:27 -0300

    Add router to display meals by category & route for no match path

--- src/Router.js
+++ src/Router.js
@@ -1,11 +1,20 @@
 import React from 'react';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
 import App from './components/App';
+import NoMatch from './components/NoMatch';
+import CategoryMeals from './components/CategoryMeals';
 
 const Routes = () => (
   <BrowserRouter>
     <Switch>
-      <Route component={App}/>
+      <Route exact path="/">
+        <Redirect to="/home" />
+      </Route>
+      <Route exact path="/home" component={App}/>
+      <Route exact path="/home/:category" component={CategoryMeals}/>
+      <Route path="*">
+        <NoMatch />
+      </Route>
     </Switch>
   </BrowserRouter>
 );
