70aaed34e3e8622904b57b437c2ed167ed6bad86
Luis Vinicius <luisvinicius0906@gmail.com>
2021-06-27 16:10:35 -0300

    Update logic to display meal card

--- src/components/CategoryMeals.js
+++ src/components/CategoryMeals.js
@@ -1,10 +1,32 @@
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom'
+import { fetchMealsByCategory } from '../state/actions';
+import MealCard from './MealCard';
 
 const CategoryMeals = () => {
+  const dispatch = useDispatch();
   const { category } = useParams();
+  const state = useSelector((state) => state)
+
+  useEffect(() => {
+    dispatch(fetchMealsByCategory(category))
+  }, [dispatch, category]);
   
+  console.log(state.data.items)
+
+  const renderMeals = () => {
+    if (state.loading) {
+      return <h1>Loading...</h1>
+    }
+    return state.data.items.map((meal) => <MealCard id={meal.idMeal} meal={meal.strMeal} image={meal.strMealThumb}/>)
+  }
+
   return (
-    <h1>Category meals of { category }</h1>
+    <>
+      <h1>Category meals of { category }</h1>
+      <p>{ renderMeals() }</p>
+    </>
   )
 };
 
