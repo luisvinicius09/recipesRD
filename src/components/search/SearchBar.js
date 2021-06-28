6a4006f5bb0cb7196b1fdedd08a59548960b7001
Luis Vinicius <luisvinicius0906@gmail.com>
2021-06-28 11:23:04 -0300

    Add logic to send data to results page

--- src/components/search/SearchBar.js
+++ src/components/search/SearchBar.js
@@ -1,5 +1,19 @@
-const SearchBar = () => (
-  <h1>Hello SearchBar</h1>
-);
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+
+const SearchBar = () => {
+  const [inputValue, setInputValue] = useState("")
+  const handleInputChange = (e) => {
+    setInputValue(e.target.value)
+  };
+
+  return (
+    <>
+      <br /><input onChange={ handleInputChange }/>
+      <Link to={{ pathname: "/search/results", inputValue }}>Submit</Link>
+      <br />
+    </>
+  );
+};
 
 export default SearchBar;
