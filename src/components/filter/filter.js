c5c04ad4f5ed3039582cffef5af7b0286540c447
Luis Vinicius <luisvinicius0906@gmail.com>
2021-06-28 14:22:12 -0300

    Add filter component

--- src/components/filter/filter.js
+++ src/components/filter/filter.js
@@ -1,5 +1,10 @@
-const filter = () => (
-  <h1>Hello Filter</h1>
+import { Link } from 'react-router-dom';
+
+const Filter = () => (
+  <div>
+    <h1>Hello Filter</h1>
+    <Link to="/filter/results">Filter</Link>
+  </div>
 );
 
-export default filter;
+export default Filter;
