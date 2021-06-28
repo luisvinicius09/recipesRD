1791e7d2932539d2413e0fba88f36f6f7be8f242
Luis Vinicius <luisvinicius0906@gmail.com>
2021-06-28 15:27:31 -0300

    Add actions to split retrieved data

--- src/state/actions/index.js
+++ src/state/actions/index.js
@@ -9,7 +9,7 @@ export const fetchCategories = () => async (dispatch, getState) => {
     // const res = await axios.get(`https://api.spoonacular.com/recipes/random?apiKey=${API_KEY}`);
     const res = await axios.get('https://themealdb.com/api/json/v1/1/categories.php');
 
-    dispatch({type: 'FETCH_SUCCESS', payload: res.data.categories });
+    dispatch({type: 'FETCH_CATEGORIES_SUCCESS', payload: res.data.categories });
   } catch (error) {
     dispatch({ type: 'FETCH_ERROR', error });
   }
@@ -20,7 +20,7 @@ export const fetchMealsByCategory = (category) => async (dispatch, getState) => 
   try {
     const res = await axios.get(`https://themealdb.com/api/json/v1/1/filter.php?c=${category}`);
 
-    dispatch({ type: 'FETCH_SUCCESS', payload: res.data.meals });
+    dispatch({ type: 'FETCH_MEALS_SUCCESS', payload: res.data.meals });
   } catch (error) {
     dispatch({ type: 'FETCH_ERROR', error });
   }
@@ -31,7 +31,7 @@ export const fetchMealById = (id) => async (dispatch, getState) => {
   try {
     const res = await axios.get(`https://themealdb.com/api/json/v1/1/lookup.php?i=${id}`);
 
-    dispatch({ type: 'FETCH_SUCCESS', payload: res.data.meals });
+    dispatch({ type: 'FETCH_MEAL_SUCCESS', payload: res.data.meals });
   } catch (error) {
     dispatch({ type: 'FETCH_ERROR', error });
   }
@@ -42,7 +42,7 @@ export const fetchMealsBySearch = (query) => async (dispatch, getState) => {
   try {
     const res = await axios.get(`https://themealdb.com/api/json/v1/1/search.php?s=${query}`);
 
-    dispatch({ type: 'FETCH_SUCCESS', payload: res.data.meals });
+    dispatch({ type: 'FETCH_SEARCH_SUCCESS', payload: res.data.meals });
   } catch (error) {
     dispatch({ type: 'FETCH_ERROR', error });
   }
