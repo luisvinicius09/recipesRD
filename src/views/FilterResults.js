d938fc9e4e982179bf7468462951bc22a5e1ee58
Luis Vinicius <luisvinicius0906@gmail.com>
2021-06-28 14:22:40 -0300

    Add page to display filter results

---
+++ src/views/FilterResults.js
@@ -0,0 +1,12 @@
+import NavBar from '../components/utils/NavBar';
+import SearchBar from '../components/search/SearchBar';
+
+const FilterResults = () => (
+  <>
+    <NavBar />
+    <SearchBar />
+    <h1>Hello Filter Results</h1>
+  </>
+);
+
+export default FilterResults;
