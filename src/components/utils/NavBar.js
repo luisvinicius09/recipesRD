899126efadc1b6c425c1d6443b6799cead442bd4
Luis Vinicius <luisvinicius0906@gmail.com>
2021-06-30 15:00:17 -0300

    Add styling to navbar

--- src/components/utils/NavBar.js
+++ src/components/utils/NavBar.js
@@ -1,10 +1,24 @@
 import { Link } from 'react-router-dom';
+import styles from '../../assets/css/Navbar.module.css';
+import SearchIcon from '../../assets/img/magnifying-glass.png';
+import EngineIcon from '../../assets/img/gear.png';
 
-const NavBar = () => (
-  <>
-    <h1>Hello NavBar</h1>
-    <Link to="/home">Home</Link>
-  </>
+const NavBar = ({ displaySearch }) => (
+  <div className={styles.container}>
+    <button className={styles.engineBtn}>
+      <img src={EngineIcon} alt="Ger" />
+    </button>
+    <Link to="/home" className={styles.title}>
+      <h1>RecipesRD</h1>
+    </Link>
+    { displaySearch ?
+      <button className={styles.displaySearch}>
+        <img src={SearchIcon} alt="Search"/>
+      </button>
+      :
+      <div className={styles.hiddenDiv}>Test</div>
+    }
+  </div>
   
 );
 
--- src/views/Home.js
+++ src/views/Home.js
@@ -23,7 +23,7 @@ const Home = () => {
 
   return (
     <>
-      <NavBar />
+      <NavBar displaySearch={true}/>
       <SearchBar />
       <Filter />
       { renderCategories() }
