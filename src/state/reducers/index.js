8ab281ee11939e316d2db1e383e3ea2eea42bb25
Luis Vinicius <luisvinicius0906@gmail.com>
2021-06-30 11:40:15 -0300

    Add filter reducer to the combine reducers function

--- src/state/reducers/index.js
+++ src/state/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import dataReducer from '../reducers/dataReducer';
+import filterReducer from './filterReducer';
 
 const reducers = combineReducers({
   data: dataReducer,
+  filter: filterReducer,
 });
 
 export default reducers;
