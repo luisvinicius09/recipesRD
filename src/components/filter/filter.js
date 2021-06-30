244d1c525cf0f7663a4d9b91aeeb27ddbe3d2d08
Luis Vinicius <luisvinicius0906@gmail.com>
2021-06-30 11:41:04 -0300

    Add filter feature to filter component

--- src/components/filter/filter.js
+++ src/components/filter/filter.js
@@ -1,10 +1,42 @@
+import { useDispatch, useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
+import { setFilter, setFilteredOptions } from '../../state/actions';
+import Input from './Input';
 
-const Filter = () => (
-  <div>
-    <h1>Hello Filter</h1>
-    <Link to="/filter/results">Filter</Link>
-  </div>
-);
+const Filter = () => {
+  const dispatch = useDispatch();
+  const state = useSelector((state) => state);
+
+  const handleInput = (e) => {
+    dispatch(setFilteredOptions(e.target.value))
+  };
+
+  const renderOptions = () => {
+    if (state.loading) {
+      return <h1>Loading...</h1>
+    }
+    const { filter } = state.filter;
+    return state.data.filterOptions
+            .filter((ingredient) => filter === '' || ingredient.strIngredient.toLowerCase().includes(filter))
+            .map((filter) => <Input key={filter.idIngredient} ingredient={filter.strIngredient}/>
+          )
+  };
+
+  const handleIngredient = (e) => {
+    dispatch(setFilter(e.target.value))
+  };
+
+  const { checked } = state.filter;
+  return (
+    <div>
+      <h1>Hello Filter</h1>
+      <input onChange={handleInput}/>
+      <div onChange={handleIngredient} style={{"overflowY": "scroll", "height": "100px"}}>
+        {renderOptions()}
+      </div>
+      <Link to={{pathname: "/filter/results", checked}}>Filter</Link>
+    </div>
+  )
+};
 
 export default Filter;
