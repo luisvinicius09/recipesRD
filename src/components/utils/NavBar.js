2c1a2ad5b8e056de0fbc6af708e5e4ec29cb9693
Luis Vinicius <luisvinicius0906@gmail.com>
2021-06-30 18:01:59 -0300

    Add logic to handle hiding search bar

--- src/components/utils/NavBar.js
+++ src/components/utils/NavBar.js
@@ -2,24 +2,27 @@ import { Link } from 'react-router-dom';
 import styles from '../../assets/css/Navbar.module.css';
 import SearchIcon from '../../assets/img/magnifying-glass.png';
 import EngineIcon from '../../assets/img/gear.png';
+import { handleModal } from '../../state/actions';
+import { useDispatch } from 'react-redux';
 
-const NavBar = ({ displaySearch }) => (
-  <div className={styles.container}>
-    <button className={styles.engineBtn}>
-      <img src={EngineIcon} alt="Ger" />
-    </button>
-    <Link to="/home" className={styles.title}>
-      <h1>RecipesRD</h1>
-    </Link>
-    { displaySearch ?
-      <button className={styles.displaySearch}>
-        <img src={SearchIcon} alt="Search"/>
+const NavBar = ({ displaySearch }) => {
+  const dispatch = useDispatch();
+  return (
+    <div className={styles.container}>
+      <button onClick={() => dispatch(handleModal(true))} className={styles.engineBtn}>
+        <img src={EngineIcon} alt="Gear" />
       </button>
-      :
-      <div className={styles.hiddenDiv}>Test</div>
-    }
-  </div>
-  
-);
-
+      <Link to="/home" className={styles.title}>
+        <h1>RecipesRD</h1>
+      </Link>
+      { displaySearch ?
+        <button className={styles.displaySearch}>
+          <img src={SearchIcon} alt="Search"/>
+        </button>
+        :
+        <div className={styles.hiddenDiv}>Test</div>
+      }
+    </div>
+  );
+}
 export default NavBar;
