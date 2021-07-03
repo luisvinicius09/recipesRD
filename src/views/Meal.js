014f0d70d06cf8253679099ea212f883211fb136
Luis Vinicius <luisvinicius0906@gmail.com>
2021-07-03 11:10:47 -0300

    Add more styling to meal by id page

--- src/views/Meal.js
+++ src/views/Meal.js
@@ -1,25 +1,40 @@
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useParams } from 'react-router';
-import { fetchMealById } from '../state/actions/index'
+import { fetchMealById, handleDetails, handleIngredients, handleRecipes } from '../state/actions/index'
 import NavBar from '../components/utils/NavBar'
 import styles from '../assets/css/MealPage.module.css';
 
 const Meal = () => {
   const dispatch = useDispatch();
   const { id } = useParams();
-  const state = useSelector((state) => state);
+  const state = useSelector((state) => state.data);
+  const details = useSelector((state) => state.details);
 
   useEffect(() => {
     dispatch(fetchMealById(id))
   }, [dispatch, id]);
 
-  const meal = state.data.meal[0];
+  const meal = state.meal[0];
 
-  if (!state.data.meal[0]) {
+  if (!meal) {
     return <h1>Loading...</h1>;
   }
-  console.log(meal)
+
+  const handleOpenRecipes = () => {
+    dispatch(handleRecipes());
+  }
+
+  const handleOpenIngredients = () => {
+    dispatch(handleIngredients());
+  }
+
+  const handleOpenDetails = () => {
+    dispatch(handleDetails());
+  }
+
+  const filteredIngredients = Object.entries(meal).filter((key) =>  /strIngredient[0-9]|&[0-9]/.test(key));
+
   return (
     <>
       <NavBar displaySearch={true} />
@@ -28,13 +43,26 @@ const Meal = () => {
       </div>
       <div className={styles.overlay}>
         <div className={styles.overlayBtns}>
-          <button type="button">Recipe</button>
-          <button type="button">Ingredients</button>
-          <button type="button"><img src="." alt="Hide" /></button>
+          <button type="button" onClick={handleOpenRecipes}>Recipe</button>
+          <button type="button" onClick={handleOpenIngredients}>Ingredients</button>
+          <button type="button" onClick={handleOpenDetails}><img src="." alt="Hide" /></button>
         </div>
-        <div className={styles.recipe}>
-          {meal.strInstructions}
-        </div>
+        {details.recipe ?
+          <div className={styles.recipe}>
+            {meal.strInstructions}
+          </div> : null
+        }
+        {details.ingredients ?
+          <div className={styles.recipe}>
+            <ul>
+              {filteredIngredients.map(element => (
+                  <li>{element[1]}</li>
+                ))
+              }
+            </ul>
+
+          </div> : null
+        }
       </div>
     </>
   )
