e8c58a14c573cbc8b29f874aba4ec538b176943c
Luis Vinicius <luisvinicius0906@gmail.com>
2021-07-03 11:22:02 -0300

    Fix image sizing on home page

--- src/components/category/CategoryCard.js
+++ src/components/category/CategoryCard.js
@@ -1,13 +1,12 @@
 import { Link } from 'react-router-dom';
 import styles from '../../assets/css/CategoryCard.module.css';
 
-const CategoryCard = ({ category, image, amount }) => (
+const CategoryCard = ({ category, image }) => (
   <div className={styles.container}>
     <Link to={"/categories/" + category}>
       <img src={ image } alt={ category }/>
       <div className={styles.description}>
         <p>{ category }</p>
-        {/* <span>{ amount }</span> */}
       </div>
     </Link>
   </div>
