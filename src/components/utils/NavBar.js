f0dc9a53b00e9c9afa154f38fba2f2575dcd2d2f
Luis Vinicius <luisvinicius0906@gmail.com>
2021-07-05 15:49:32 -0300

    Update gear image to link to ingredients page

--- src/components/utils/NavBar.js
+++ src/components/utils/NavBar.js
@@ -2,16 +2,13 @@ import { Link } from 'react-router-dom';
 import styles from '../../assets/css/Navbar.module.css';
 import SearchIcon from '../../assets/img/magnifying-glass.png';
 import EngineIcon from '../../assets/img/gear.png';
-import { handleModal } from '../../state/actions';
-import { useDispatch } from 'react-redux';
 
 const NavBar = ({ displaySearch }) => {
-  const dispatch = useDispatch();
   return (
     <div className={styles.container}>
-      <button onClick={() => dispatch(handleModal(true))} className={styles.engineBtn}>
+      <Link to="/filter" className={styles.engineBtn}>
         <img src={EngineIcon} alt="Gear" />
-      </button>
+      </Link>
       <Link to="/home" className={styles.title}>
         <h1>RecipesRD</h1>
       </Link>
